158527db6e7e75381084c33f7277460335fdf2ab
David <119945403+davidpd7@users.noreply.github.com>
2023-09-05 10:34:30 +0100

    installing

--- definitive app/cubematchfinance/__main__.py
+++ definitive app/cubematchfinance/__main__.py
@@ -4,9 +4,12 @@ from cubematchfinance.app import App
 def main(args = None):
     if args is None:
         args = sys.argv[1:]
-
+    
     app = App()
+    app.show()
     app.run()
 
 if __name__ == '__main__':
     sys.exit(main())
+    
+    
